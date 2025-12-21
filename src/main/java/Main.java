bb1fb5fece4cef9f471963e2236e29e2d08f1aee
stavteresh <stav.teresh@gmail.com>
2025-12-21 15:06:34 +0200

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,12 +2,22 @@ public class Main
 {
 	public static void main(String[] args) {
 		Queue<Integer> q = new Queue<Integer>();
-	    q.insert(9);
+	    q.insert(7);
+	    q.insert(7);
 	    q.insert(2);
 	    q.insert(5);
 	    
+	    Queue<Integer> q2 = new Queue<Integer>();
+	    q2.insert(21);
+	    q2.insert(15);
+	    q2.insert(30);
+	    
 	    System.out.println(ex1(q));
 	    System.out.println(ex2(q));
+	    System.out.println(ex3(q, 45));
+	    System.out.println(ex4(q, q2));
+	    System.out.println(ex5(q, 7));
+	    
 	}
 	
 	public static Queue<Integer> ex1(Queue<Integer> q)
@@ -28,21 +38,71 @@ public class Main
 	
 
 	
-	public static int ex2(Queue<Integer> h)
+	public static double ex2(Queue<Integer> h)
 	{
-      Queue<Integer> q = ex1(h);
-	    int avg = 0;
-	    int sum = 0;
-	    Queue<Integer> h = q;
+       Queue<Integer> q = ex1(h);
+	    double sum = 0;
+	    int count = 0;
 	    while(!q.isEmpty())
 	    {
-	        avg = avg + q.remove();
-	        sum++;
+	        sum = sum + q.remove();
+	        count++;
 	    }
-	    return avg/sum;
+	    return sum/count;
 	}
 	
+	public static int ex3(Queue<Integer> h, int x)
+	{
+	    Queue<Integer> q = ex1(h);
+	    int count = 0;
+	    while(!q.isEmpty())
+	    {
+	        if(x%q.remove()==0)
+	            count++;
+	    }
+	    return count;
+	}
 	
+	public static boolean ex4(Queue<Integer> h1, Queue<Integer> h2)
+	{
+	    Queue<Integer> q1 = ex1(h1);
+	    Queue<Integer> q2 = ex1(h2);
+	    boolean condition;
+	    while(!q1.isEmpty())
+	    {
+	        q2 = ex1(h2);
+	        condition = false;
+	        while(!q2.isEmpty())
+	        {
+	            if(q2.remove()%q1.head()==0)
+	                condition = true;
+	        }
+	        
+	        q1.remove();
+	        
+	        if(!condition)
+	        {
+	            return false;
+	        }
+	    }
+	    return true;
+	   
+	}
+	
+	public static boolean ex5(Queue<Integer> h1, int x)
+	{
+	    Queue<Integer> q1 = ex1(h1);
+	    while(!q1.isEmpty())
+	    {
+	        int k = q1.remove();
+	        if(q1.isEmpty())
+	            return false;
+	        if(k==q1.head()&&q1.head() == x)
+	            return true;
+	    }
+	    return false;
+	}
+
 	
 	
 }
